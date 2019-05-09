b70a25fe0ba54a2b4c96ebe0ed5febe2eb9f913e
Gabriele <gabboxl0@gmail.com>
2019-05-09 21:40:30 +0200

    fix parent activity MainActivity recreated when clicked back actionbar button

--- app/src/main/java/ga/gabboxl/pininorario/SettingsActivity.kt
+++ app/src/main/java/ga/gabboxl/pininorario/SettingsActivity.kt
@@ -17,6 +17,15 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed() // or finish();
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.app_preferences, rootKey)
