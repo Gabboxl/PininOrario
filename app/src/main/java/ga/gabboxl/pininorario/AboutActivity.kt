47ef9e17bc7fa96299159555cf0617204e136686
Gabriele <gabboxl0@gmail.com>
2020-02-17 21:47:39 +0100

    Added author about cards + using Iconics for icons

--- app/src/main/java/ga/gabboxl/pininorario/AboutActivity.kt
+++ app/src/main/java/ga/gabboxl/pininorario/AboutActivity.kt
@@ -1,6 +1,8 @@
 package ga.gabboxl.pininorario
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.danielstone.materialaboutlibrary.MaterialAboutActivity
@@ -10,16 +12,22 @@ import com.danielstone.materialaboutlibrary.model.MaterialAboutList
 import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.enums.Display
 import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.mikepenz.iconics.IconicsColor
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
+import com.mikepenz.iconics.utils.sizeDp
 
 
 class AboutActivity : MaterialAboutActivity() {
     override fun getMaterialAboutList(context: Context): MaterialAboutList {
 
         val miscCardBuilder: MaterialAboutCard.Builder = MaterialAboutCard.Builder()
+        val authorCardBuilder: MaterialAboutCard.Builder = MaterialAboutCard.Builder()
+        buildAuthor(context, authorCardBuilder)
         buildMisc(context, miscCardBuilder)
 
 
-        return MaterialAboutList(miscCardBuilder.build()) // This creates an empty screen, add cards with .addCard()
+        return MaterialAboutList(miscCardBuilder.build(), authorCardBuilder.build()) // This creates an empty screen, add cards with .addCard()
     }
 
     private fun buildMisc(context: Context, miscCardBuilder: MaterialAboutCard.Builder){
@@ -42,6 +50,32 @@ class AboutActivity : MaterialAboutActivity() {
                 .subText(R.string.credits_about)
                 .build())
 
+    }
+
+    private fun buildAuthor(context: Context, authorCardBuilder: MaterialAboutCard.Builder) {
+        authorCardBuilder.title("Autore")
+        authorCardBuilder.addItem(MaterialAboutActionItem.Builder()
+            .text("Gabriele")
+            .subText("gabboxl")
+            .icon(IconicsDrawable(context)
+                .icon(CommunityMaterial.Icon.cmd_account)
+                .color(IconicsColor.colorInt(ContextCompat.getColor(context, R.color.mal_color_icon_light_theme)))
+                .sizeDp(18))
+            .setOnClickAction{
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse("https://gabboxl.ga")
+                    context.startActivity(i) }
+            .build())
+            .addItem(MaterialAboutActionItem.Builder()
+                .text("PininOrario su Github")
+                .icon(IconicsDrawable(context)
+                    .icon(CommunityMaterial.Icon.cmd_github_circle)
+                    .color(IconicsColor.colorInt(ContextCompat.getColor(context, R.color.mal_color_icon_light_theme)))
+                    .sizeDp(18))
+                .setOnClickAction{val i = Intent(Intent.ACTION_VIEW)
+                    i.data = Uri.parse("https://github.com/gabboxl/PininOrario")
+                    context.startActivity(i)}
+                .build())
     }
 
 
