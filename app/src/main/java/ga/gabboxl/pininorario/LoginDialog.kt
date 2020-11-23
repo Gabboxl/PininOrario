b1643ac8d629fdbdbb7b0868f23284bb436c1fc6
Gabriele <gabboxl0@gmail.com>
2020-11-23 14:10:42 +0100

    using ViewBinding for LoginDialog

--- app/src/main/java/ga/gabboxl/pininorario/LoginDialog.kt
+++ app/src/main/java/ga/gabboxl/pininorario/LoginDialog.kt
@@ -3,44 +3,42 @@ package ga.gabboxl.pininorario
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
 import es.dmoral.toasty.Toasty
+import ga.gabboxl.pininorario.databinding.LoginDialogLayoutBinding
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 
 class LoginDialog : DialogFragment() {
+    private lateinit var binding: LoginDialogLayoutBinding
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val inflater: LayoutInflater = requireActivity().layoutInflater
-        val viewLayoutDialog: View = inflater.inflate(R.layout.login_dialog_layout, null)
+        binding = LoginDialogLayoutBinding.inflate(layoutInflater)
 
-        viewLayoutDialog.findViewById<EditText>(R.id.edit_username).setText(sharedPreferences.getString("pinin_username", ""))
-        viewLayoutDialog.findViewById<EditText>(R.id.edit_password).setText(sharedPreferences.getString("pinin_password", ""))
+        binding.editUsername.setText(sharedPreferences.getString("pinin_username", ""))
+        binding.editPassword.setText(sharedPreferences.getString("pinin_password", ""))
 
 
-        val dialog = AlertDialog.Builder(context).setView(viewLayoutDialog)
+        val dialog = AlertDialog.Builder(context).setView(binding.root)
             .setTitle(getString(R.string.login))
             .setNegativeButton(getString(R.string.annulla_button)) { dialog, which ->
 
             }
             .setNeutralButton(getString(R.string.test_login_btn), null)
             .setPositiveButton(getString(R.string.salva)) { dialog, which ->
-                sharedPreferences.edit().putString("pinin_username", viewLayoutDialog.findViewById<EditText>(R.id.edit_username).text.toString()).apply()
-                sharedPreferences.edit().putString("pinin_password", viewLayoutDialog.findViewById<EditText>(R.id.edit_password).text.toString()).apply()
+                sharedPreferences.edit().putString("pinin_username", binding.editUsername.text.toString()).apply()
+                sharedPreferences.edit().putString("pinin_password", binding.editPassword.text.toString()).apply()
             }
             .show()
 
         dialog.getButton(Dialog.BUTTON_NEUTRAL).setOnClickListener {
             lifecycleScope.launch(Main) {
                 val textusername =
-                    viewLayoutDialog.findViewById<EditText>(R.id.edit_username).text.toString()
+                    binding.editUsername.text.toString()
                 val textpassword =
-                    viewLayoutDialog.findViewById<EditText>(R.id.edit_password).text.toString()
+                    binding.editPassword.text.toString()
                 if (OrariUtils.checkLogin(context, textusername, textpassword)) {
                     Toasty.success(requireContext(), getString(R.string.login_riuscito), Toasty.LENGTH_SHORT).show()
                  } else {
