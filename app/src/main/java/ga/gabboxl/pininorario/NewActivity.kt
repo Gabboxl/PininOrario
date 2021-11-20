94457815f6fa1730473d26d4998517f9aa76b3b6
Gabriele <gabboxl0@gmail.com>
2021-11-20 14:41:43 +0100

    Added NewActivity, a plus icon and make it the default activity for testing purposes

---
+++ app/src/main/java/ga/gabboxl/pininorario/NewActivity.kt
@@ -0,0 +1,11 @@
+package ga.gabboxl.pininorario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
