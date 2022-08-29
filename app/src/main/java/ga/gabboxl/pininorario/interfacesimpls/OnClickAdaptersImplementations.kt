d727644d94af5e97eabe847a072cc41e06b1dec2
Gabriele <gabboxl0@gmail.com>
2022-08-30 01:31:20 +0200

    Implemented timetable download logic
    Thanks to okhttp && okio

--- app/src/main/java/ga/gabboxl/pininorario/interfacesimpls/OnClickAdaptersImplementations.kt
+++ app/src/main/java/ga/gabboxl/pininorario/interfacesimpls/OnClickAdaptersImplementations.kt
@@ -1,22 +1,86 @@
 package ga.gabboxl.pininorario.interfacesimpls
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
-import ga.gabboxl.pininorario.Classe
-import ga.gabboxl.pininorario.ClasseViewModel
-import ga.gabboxl.pininorario.ClasseWithPeriodi
-import ga.gabboxl.pininorario.Periodo
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
+import androidx.lifecycle.viewModelScope
+import es.dmoral.toasty.Toasty
+import ga.gabboxl.pininorario.*
 import ga.gabboxl.pininorario.adapters.ClasseAdapter
 import ga.gabboxl.pininorario.adapters.PeriodoAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.*
+import okio.BufferedSink
+import okio.Okio
+import okio.buffer
+import okio.sink
+import java.io.File
+import java.io.IOException
+
 
 class OnClickAdaptersImplementations(val context : Context?, private val classeViewModel: ClasseViewModel) : PeriodoAdapter.OnClickListenersPeriodoAdapter, ClasseAdapter.OnClickListenersClasseAdapter {
 
     override fun onPeriodoScaricaButtonClick(periodo: Periodo) {
         Toast.makeText(
             context,
-            "darkkk: " + periodo.nomePeriodo + "\n classe: ",
+            "lesgo: " + "\n classe: ",
             Toast.LENGTH_SHORT
         ).show()
+
+
+/*      Con downloadmanager non è possibile salvare nella cartella dedicata dell'app: https://stackoverflow.com/a/71341789/9008381
+
+
+        var nomefileOrario = periodo.periodoSemiLinkImg
+
+        val url = Uri.parse("https://orario.itispininfarina.it/classi/" + periodo.periodoSemiLinkImg + ".png")
+
+        val file = File(context?.filesDir, "darkside.png")
+
+        val request = DownloadManager.Request(url)
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+        request.setDestinationUri(Uri.fromFile(file))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setAllowedOverRoaming(true)
+        request.setAllowedOverMetered(true)
+        request.setTitle("PininOrario - $nomefileOrario.png")
+        request.setDescription("In download $nomefileOrario.png")
+
+        val downloadManager: DownloadManager = context?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+        downloadManager.enqueue(request)*/
+
+        classeViewModel.viewModelScope.launch(Dispatchers.Default) {
+            //scarico l'immagine con okhttp
+            val clientok = OkHttpClient()
+            val reqimg = Request.Builder()
+                .url("https://orario.itispininfarina.it/classi/" + periodo.periodoSemiLinkImg + ".png")
+                .get()
+                .build()
+            val respok = clientok.newCall(reqimg).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                    Toasty.error(context!!, "haha yes $e", Toasty.LENGTH_SHORT).show()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val filex = File(context?.filesDir, "darkside.png")
+                    if (filex.exists()) {
+                        //nice
+                    }
+                    val fileCreated: Boolean = filex.createNewFile()
+                    val sink: BufferedSink = filex.sink().buffer()
+                    sink.writeAll(response.body!!.source())
+                    sink.close()
+                }
+            }
+            )
+
+
+
+        }
 
 
         classeViewModel.updatePeriodo(
